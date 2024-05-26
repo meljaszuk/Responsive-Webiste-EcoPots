c253798b6273293212f20613579f9f17e62f017d
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-05-26 22:21:58 +0200

    cart feature added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -55,3 +55,67 @@ function handleResize() {
 }
 
 window.addEventListener('resize', handleResize);
+
+document.querySelector('.not-ready').addEventListener('click', function() {
+  document.querySelector('.not-ready-text').style.display = 'block';
+});
+
+let volumeLargePot = 0;
+let volumeSmallPot = 0;
+let valueLargePot = 0;
+let valueSmallPot = 0;
+let valueTotal = 0;
+let volumeTotal = 0;
+const PRICE_SMALL_POT = 4.5;
+const PRICE_LARGE_POT = 7.5;
+
+function updateLargePot(item) {
+  volumeLargePot = volumeLargePot + item;
+  valueLargePot = volumeLargePot * PRICE_LARGE_POT;
+  updateTotal();
+  document.querySelector('.basket__volume-large-pot')
+    .textContent = volumeLargePot;
+
+  document.querySelector('.basket__value-large-pot')
+    .textContent = `$ ` + valueLargePot.toFixed(2);
+}
+
+function updateSmallPot(item) {
+  volumeSmallPot = volumeSmallPot + item;
+  valueSmallPot = volumeSmallPot * PRICE_SMALL_POT;
+  updateTotal();
+
+  document.querySelector('.basket__volume-small-pot')
+    .textContent = volumeSmallPot;
+
+  document.querySelector('.basket__value-small-pot')
+    .textContent = `$ ` + valueSmallPot.toFixed(2);
+}
+
+function updateTotal() {
+  valueTotal = valueSmallPot + valueLargePot;
+  volumeTotal = volumeLargePot + volumeSmallPot;
+  document.querySelector('.basket__volume-total').textContent = volumeTotal;
+  document.querySelector('.basket__value-total').textContent = `$ ` + valueTotal.toFixed(2);
+  document.querySelector('.header__product-number').textContent = volumeTotal;
+}
+
+document.querySelector('.basket__plus.small-pot')
+  .addEventListener('click', function() {
+    updateSmallPot(1);
+  });
+
+document.querySelector('.basket__minus.small-pot')
+  .addEventListener('click', function() {
+    updateSmallPot(-1);
+  });
+
+document.querySelector('.basket__plus.large-pot')
+  .addEventListener('click', function() {
+    updateLargePot(1);
+  });
+
+document.querySelector('.basket__minus.large-pot')
+  .addEventListener('click', function() {
+    updateLargePot(-1);
+  });
